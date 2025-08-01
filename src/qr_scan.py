8842c6a4ce8efeff8439b76d77bc5d7567eb0905
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-01 11:12:49 +0900

    QR코드에 텍스트 함께 출력하기

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -14,9 +14,14 @@ decoded = pyzbar.decode(gray)
 print(decoded)
 for d in decoded:
     print(d.data.decode("utf-8"))
+    barcode_data = d.data.decode('utf-8')
     print(d.type)
+    barcode_type = d.type
+    text = '%s (%s)' % (barcode_data, barcode_type)
 
-    cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]),(0,255,0), 5)
+    cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]),(0,255,0), 20)
+    cv2.putText(img, text, (d.rect[0], d.rect[1] - 80), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,0), 10, cv2.LINE_AA)
+
 
 plt.imshow(img)
 plt.show()
