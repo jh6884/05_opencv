0323df7bfe7d7442cc7e6012bd485a32533beca9
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-01 10:40:40 +0900

    디코드, 확인

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -9,5 +9,9 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap="gray")
 plt.show()
 
+# 디코딩
+decoded = pyzbar.decode(gray)
+print(decoded)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
